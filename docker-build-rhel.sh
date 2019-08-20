7100c06b4423f16baf19d37f69f4c370d3ff7ba9
Chance Zibolski <czibolsk@redhat.com>
2019-08-20 09:01:27 -0700

    Update docker-build-rhel.sh to use new REPO_DIR var

---
+++ docker-build-rhel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+: "${REPO_DIR:?}"
+: "${SUB_MGR_FILE:?}"
+: "${IMAGE_TAG:=latest}"
+
+REPO_MNT="$REPO_DIR:/etc/yum.repos.d"
+SUB_MGR_MNT="$SUB_MGR_FILE:/etc/yum/pluginconf.d/subscription-manager.conf"
+imagebuilder \
+    -mount "$REPO_MNT" \
+    -mount "$SUB_MGR_MNT" \
+    -t "registry.access.redhat.com/openshift/ose-metering-hadoop:$IMAGE_TAG" \
+    -f "$DIR/Dockerfile.rhel" \
+    "$DIR"
+
+docker tag \
+    "registry.access.redhat.com/openshift/ose-hadoop:$IMAGE_TAG" \
+    "openshift/ose-metering-hadoop:$IMAGE_TAG"
